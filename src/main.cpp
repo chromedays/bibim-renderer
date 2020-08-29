04c70ece40972a89a1a9181361c43629be16a192
Ilgwon Ha <haik0925@gmail.com>
2020-08-29 15:40:45 -0700

    enabled validation layers

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include "external/SDL2/SDL.h"
 #include "external/SDL2/SDL_main.h"
 #include "external/SDL2/SDL_syswm.h"
+#include "vulkan/vulkan_core.h"
+#include <vector>
 #include <stdio.h>
 #include <assert.h>
 
@@ -22,13 +24,65 @@ int main(int _argc, char **_argv) {
   BB_VK_ASSERT(volkInitialize());
 
   SDL_Init(SDL_INIT_VIDEO);
+  int width = 1280;
+  int height = 720;
   SDL_Window *window =
       SDL_CreateWindow("Bibim Renderer", SDL_WINDOWPOS_CENTERED,
-                       SDL_WINDOWPOS_CENTERED, 1280, 720, 0);
+                       SDL_WINDOWPOS_CENTERED, width, height, 0);
 
   SDL_SysWMinfo sysinfo = {};
   SDL_VERSION(&sysinfo.version);
   SDL_GetWindowWMInfo(window, &sysinfo);
+
+  VkInstance instance;
+  VkApplicationInfo appinfo = {};
+  appinfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+  appinfo.pApplicationName = "Bibim Renderer";
+  appinfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+  appinfo.pEngineName = "Bibim Renderer";
+  appinfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+  appinfo.apiVersion = VK_API_VERSION_1_2;
+  VkInstanceCreateInfo instanceCreateInfo = {};
+  instanceCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+  instanceCreateInfo.pApplicationInfo = &appinfo;
+
+  std::vector<const char *> validationLayers = {"VK_LAYER_KHRONOS_validation"};
+
+  bool enableValidationLayers =
+#ifdef BB_DEBUG
+      true;
+#else
+      false;
+#endif
+
+  uint32_t numLayers;
+  vkEnumerateInstanceLayerProperties(&numLayers, nullptr);
+  std::vector<VkLayerProperties> layerProperties(numLayers);
+  vkEnumerateInstanceLayerProperties(&numLayers, layerProperties.data());
+  bool canEnableLayers = true;
+  for (const char *layerName : validationLayers) {
+    bool foundLayer = false;
+    for (VkLayerProperties &properties : layerProperties) {
+      if (strcmp(properties.layerName, layerName) == 0) {
+        foundLayer = true;
+        break;
+      }
+    }
+
+    if (!foundLayer) {
+      canEnableLayers = false;
+      break;
+    }
+  }
+
+  if (enableValidationLayers && canEnableLayers) {
+    instanceCreateInfo.enabledLayerCount = (uint32_t)validationLayers.size();
+    instanceCreateInfo.ppEnabledLayerNames = validationLayers.data();
+  }
+
+  BB_VK_ASSERT(vkCreateInstance(&instanceCreateInfo, nullptr, &instance));
+
+  volkLoadInstance(instance);
 
   bool running = true;
   SDL_Event e = {};
@@ -42,6 +96,8 @@ int main(int _argc, char **_argv) {
     // update(1.f / 60.f);
     // draw();
   }
+
+  vkDestroyInstance(instance, nullptr);
 
   SDL_DestroyWindow(window);
   SDL_Quit();
