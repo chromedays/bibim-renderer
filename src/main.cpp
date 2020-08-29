f98c7fe77df322bc681d1d7adaa64dce6e33672d
Ilgwon Ha <haik0925@gmail.com>
2020-08-29 14:21:51 -0700

    added volk

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
+#include "external/volk.h"
 #include <stdio.h>
+#include <assert.h>
+
+#if BB_DEBUG
+#define BB_ASSERT(exp) assert(exp)
+#else
+#define BB_ASSERT(exp)
+#endif
+#define BB_VK_ASSERT(exp)                                                      \
+  do {                                                                         \
+    auto result = exp;                                                         \
+    BB_ASSERT(result == VK_SUCCESS);                                           \
+  } while (0)
+
+namespace bb {}
 
 int main(int argc, char **argv) {
+  BB_VK_ASSERT(volkInitialize());
   printf("Hello, World!");
   return 0;
 }
