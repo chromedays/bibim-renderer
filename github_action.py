3b404a147d17ccbd6c249fc3fd711a61d0842d9f
Ilgwon Ha <haik0925@gmail.com>
2020-09-06 23:48:49 -0700

    capture outputs of all subprocesses

--- github_action.py
+++ github_action.py
@@ -1,12 +1,32 @@
 import subprocess
 
+
+def run_command(cmd: list):
+    cmd_str = f"'{' '.join(cmd)}'"
+    print(f"Running {cmd_str}...")
+    process = subprocess.run(
+        cmd, shell=True, capture_output=True, universal_newlines=True
+    )
+
+    stdout = process.stdout.strip()
+    stderr = process.stderr.strip()
+    if len(stdout) > 0:
+        for line in stdout.splitlines():
+            print(f"{cmd_str} (STDOUT): {line}")
+    if len(stderr) > 0:
+        for line in stderr.splitlines():
+            print(f"{cmd_str} (STDERR): {line}")
+
+    if process.returncode == 0:
+        print("Done.")
+    else:
+        raise Exception(f"{cmd_str} failed.")
+
+
 def main():
-    print('Generating path.gen.bff...')
-    dir = subprocess.run(['dir'], shell=True, capture_output=True)
-    print(dir.stdout)
-    # subprocess.run(['python', 'gen_config.py'], shell=True, universal_newlines=True)
-    print('Running FASTBuild...')
-    # subprocess.run(['FBuild.exe'], shell=True, universal_newlines=True)
+    run_command(["python", "gen_config.py"])
+    run_command(["FBuild.exe"])
+
 
 if __name__ == "__main__":
     main()
