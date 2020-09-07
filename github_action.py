84c6627cbf3f59a9f1141ce02a5fc24c00a0d3b4
Ilgwon Ha <haik0925@gmail.com>
2020-09-06 23:20:13 -0700

    testing

--- github_action.py
+++ github_action.py
@@ -2,6 +2,7 @@ import subprocess
 
 def main():
     print('Generating path.gen.bff...')
+    subprocess.run(['dir'], shell=True)
     # subprocess.run(['python', 'gen_config.py'], shell=True, universal_newlines=True)
     print('Running FASTBuild...')
     # subprocess.run(['FBuild.exe'], shell=True, universal_newlines=True)
