c179ab5710a5bbe16abc3601145f573f9f764fee
Ilgwon Ha <haik0925@gmail.com>
2020-09-06 23:31:51 -0700

    capture and print the output from subprocesses

--- github_action.py
+++ github_action.py
@@ -2,7 +2,8 @@ import subprocess
 
 def main():
     print('Generating path.gen.bff...')
-    subprocess.run(['dir'], shell=True)
+    dir = subprocess.run(['dir'], shell=True, capture_output=True)
+    print(dir.stdout)
     # subprocess.run(['python', 'gen_config.py'], shell=True, universal_newlines=True)
     print('Running FASTBuild...')
     # subprocess.run(['FBuild.exe'], shell=True, universal_newlines=True)
