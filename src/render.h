b55d04e477989c6a2ae1cfbf0a652f52abf50fc5
Ilgwon Ha <haik0925@gmail.com>
2020-10-27 05:17:31 -0700

    clang-format

--- src/render.h
+++ src/render.h
@@ -277,10 +277,9 @@ StandardPipelineLayout createStandardPipelineLayout(const Renderer &_renderer);
 void destroyStandardPipelineLayout(const Renderer &_renderer,
                                    StandardPipelineLayout &_layout);
 
-VkDescriptorPool
-createStandardDescriptorPool(const Renderer &_renderer,
-                             const StandardPipelineLayout &_layout,
-                             const EnumArray<DescriptorFrequency, uint32_t>& _numSets);
+VkDescriptorPool createStandardDescriptorPool(
+    const Renderer &_renderer, const StandardPipelineLayout &_layout,
+    const EnumArray<DescriptorFrequency, uint32_t> &_numSets);
 
 struct alignas(16) Light {
   Float3 Pos;
